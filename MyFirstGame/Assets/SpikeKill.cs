84e880176f769ecc85800f3b89441db7ab6a9745
STUDENT Callum Reay <Callum.Reay@students.ittralee.ie>
2023-11-23 14:05:46 +0000

    got spike to kill zombie
    Signed-off-by: STUDENT Callum Reay <Callum.Reay@students.ittralee.ie>

--- MyFirstGame/Assets/SpikeKill.cs
+++ MyFirstGame/Assets/SpikeKill.cs
@@ -2,21 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enemyspikeballControl : MonoBehaviour
+public class spikeKill : MonoBehaviour
+
 {
-    private Vector3 dir = Vector3.up;
-    private object enemyspikeKill;
+    float SpikeKill = 10f;
+
+    public spikeKill(float spikeKill)
+    {
+        SpikeKill = spikeKill;
+    }
+
+    private Vector3 dir;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.DownArrow))
+
+        if (Input.GetKey(KeyCode.Space))
             transform.position += (new Vector3(0, 1, 2)) * Time.deltaTime;
         if (Input.GetKey(KeyCode.W))
             transform.position += (new Vector3(0, 1, 2)) * Time.deltaTime;
@@ -27,15 +35,10 @@ public class enemyspikeballControl : MonoBehaviour
         {
             dir = Vector3.up;
         }
-        else if (transform.position.y >= 4)
+        else if (transform.position.y >= 3)
         {
             dir = Vector3.down;
         }
-        {
-            
-        }
-        }
-
+        
     }
-    
-
+}
